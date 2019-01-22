8edefb3b72c14b1438a6f8d5ea59bd74e87c3696
thiago alvaro <thiagodiniz811@gmail.com>
2019-01-22 18:45:58 -0200

    implementando cartao

--- src/main/java/pagamentossa/service/ClienteService.java
+++ src/main/java/pagamentossa/service/ClienteService.java
@@ -2,6 +2,7 @@ package pagamentossa.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,24 +15,36 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	public void createCliente(Cliente cliente) {
 		clienteRepository.save(cliente);
 	}
-	
-	public List<Cliente> readAllCliente(){
+
+	public List<Cliente> readAllCliente() {
 		return clienteRepository.findAll();
 	}
-	
+
 	public Optional<Cliente> readById(Long id) {
 		return clienteRepository.findById(id);
 	}
-	
+
 	public void updateCliente(Cliente cliente) {
 		clienteRepository.save(cliente);
 	}
-	
+
 	public void deleteCliente(Long id) {
 		clienteRepository.deleteById(id);
+	}
+
+	public String getNumCartao(String cpf, String bin) {
+		int max = 100000000;
+		int min = 0;
+		String parte1 = cpf.substring(0, 4);
+		parte1 = parte1.concat(bin);
+
+		Random gerador = new Random();
+		int num = gerador.nextInt((max - min) + min);
+		parte1 = parte1 + num;
+		return parte1;
 	}
 }
